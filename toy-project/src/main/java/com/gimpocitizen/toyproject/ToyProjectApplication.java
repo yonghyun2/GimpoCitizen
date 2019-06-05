51d6b1b4f2e54434aad36c08fcc97c830da23e72
wntjdjus <wntjdjus@naver.com>
2019-06-05 14:40:36 +0900

    [ADD] first

---
+++ toy-project/src/main/java/com/gimpocitizen/toyproject/ToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gimpocitizen.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectApplication.class, args);
+    }
+
+}
---
+++ toy-project/src/test/java/com/gimpocitizen/toyproject/ToyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gimpocitizen.toyproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToyProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
